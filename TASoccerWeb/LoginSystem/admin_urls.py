4dce817eb67ac1e9df830291d50d918f08eff5c4
Peerapas <peerapas.sr@kkumail.com>
2025-03-22 03:13:05 +0700

    update

--- TASoccerWeb/LoginSystem/admin_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import LoginViews
-
-urlpatterns = [
-    path('dashboard/', LoginViews.admin_dashboard, name='admin_dashboard'),
-    path('user/', LoginViews.admin_userManagement, name='admin_userManagement'),
-    path('reservation/', LoginViews.admin_reservaionManagement, name='admin_reservationManagement'),
-    path("admin-gallery/", LoginViews.admin_gallery, name="admin-gallery"),
-]
